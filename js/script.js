7b7ab8935b936f82c9f47b85594230b086e7a76f
OtavioRV <otavioreisv@gmail.com>
2021-03-16 16:01:49 -0300

    ANIMACOES JS FINALIZADAS

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ new SimpleSlide({
 	 time: 5000,
 	nav: true
  });}
+ if(window.SimpleAnime){
+	new SimpleAnime();}
+	
+	if(window.SimpleForm){
+	new SimpleForm({
+		form:".formphp", 
+		button:"#enviar",
+		erro:"<div id='form-erro'> <h3>Erro ao enviar!</h3><p> Um erro ocorreu, tente enviar um email para contato@bikecraft.com </p></div>",
+		sucesso:"<div id='form-sucesso'> <h2> Sucesso ao Enviar!</h2> <p> Em breve entraemos em contato.</p></div>",
+	});
+	}
